03c1235a2c181c38c70b367797d5e58543b73f1e
soabel <alfredo.benaute@gmail.com>
2021-04-07 17:52:58 -0500

    add project

---
+++ src/test/java/com/bcp/bootcamp/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcp.bootcamp.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
